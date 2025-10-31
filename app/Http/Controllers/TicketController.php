01a097eed6b70b5d6b32811ac49aa4829bc03d99
deven-aditya <230712333@students.uajy.ac.id>
2025-10-31 12:15:13 +0700

    innit commit

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -19,4 +19,43 @@ class TicketController extends Controller
         $tickets = Ticket::latest()->paginate(5);  
         return view('ticket.index', compact('tickets')); 
     } 
+
+    /**
+     * create
+     *
+     * @return void
+     */
+    public function create()
+    {
+        return view('ticket.create');
+    }
+
+    /**
+     * store
+     *
+     * @param Request $request
+     * @return void
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'id_event' => 'required',
+            'ticket_type' => 'required',
+            'price' => 'required|decimal'
+        ]);
+
+        Ticket::create([
+            'id_event' => $request->id_event,
+            'ticket_type' => $request->ticket_type,
+            'price' => $request->price
+        ]);
+
+        try {
+            return redirect()->route('ticket.index')->with(['success' => 'Data Berhasil Disimpan!']);
+        } catch (Exception $e) {
+            return redirect()->route('ticket.index')->with(['error' => $e->getMessage()]);
+        }
+    }
+
+    
 } 
